924afebb5731aa1856380af59f1b012049bada68
Natalia Dushkina <natalia.dushkina@groupcdg.com>
2023-02-06 14:40:06 +0000

    init app

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import * as Sentry from '@sentry/angular';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'myApp';
+
+  onClickMeClick() {
+    alert('You clicked me!');
+    Sentry.captureException(new Error('You clicked me!'));
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { environment } from 'environment';
+import * as Sentry from '@sentry/angular';
+
+Sentry.init({
+  dsn: environment.dsn,
+  release: environment.version,
+});
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
