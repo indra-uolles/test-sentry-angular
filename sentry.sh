8a83fa49710031a87471dc843b45906d6a38e751
Natalia Dushkina <natalia.dushkina@groupcdg.com>
2023-02-08 09:25:22 +0000

    update sh script

--- sentry.sh
+++ sentry.sh
@@ -1,7 +1,7 @@
 sentry-cli releases new -p test-sentry-angular "$1"
 sentry-cli releases set-commits --auto "$1"
-sentry-cli releases files "$1" upload docs/
-sentry-cli releases files "$1" upload-sourcemaps /docs
+sentry-cli releases files "$1" upload docs/ --ext js --url-prefix https://effervescent-alfajores-e7d3ad.netlify.app
+sentry-cli releases files "$1" upload-sourcemaps docs/ --ext map --url-prefix https://effervescent-alfajores-e7d3ad.netlify.app --rewrite
 sentry-cli releases new "$1" --finalize
 
 # empty source maps for safety. this is a workaround for a bug in sentry-cli
